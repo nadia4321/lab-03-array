27f9fc12eae3e6e8ef1ec4711f121adfcecf46e9
nadia <104782116+nadia4321@users.noreply.github.com>
2023-10-10 13:19:52 +0300

    Update task_2.cpp

--- sources/task_2.cpp
+++ sources/task_2.cpp
@@ -4,31 +4,35 @@
 
 // Task 2.
 int sum(const std::vector<int>& input) {
-    // Fix it.
-    return 0;
+    int sum_num = 0;
+    for(size_t i = 0; i < input.size(); ++i) sum_num += input[i];
+    return sum_num;
 }
 
 int square_sum(const std::vector<int>& input) {
-    // Fix it.
-    return 0;
+    int sum_num = 0;
+    for(size_t i = 0; i < input.size(); ++i) sum_num += input[i] * input[i]; 
+    return sum_num;
 }
 
 int sum_six(const std::vector<int>& input) {
-    // Fix it.
-    return 0;
+    int sum_num = 0;
+    for(size_t i = 0; i < 6; ++i) sum_num += input[i];
+    return sum_num;
 }
 
 int sum_k(const std::vector<int>& input, int k1, int k2) {
-    // Fix it.
-    return 0;
+    int sum_num = 0;
+    for(size_t i = k1; i <= k2; ++i) sum_num += input[i];
+    return sum_num;
 }
 
 int mean(const std::vector<int>& input) {
-    // Fix it.
-    return 0;
+    
+    return sum(input) / input.size();
 }
 
 int mean_k(const std::vector<int>& input, int s1, int s2) {
-    // Fix it.
-    return 0;
+    
+    return sum_k(input, s1, s2) / (s2 - s1 + 1);
 }
