9c5ae11675e0e5cec920f17bb889fb472e1b3009
nadia <104782116+nadia4321@users.noreply.github.com>
2023-10-11 23:00:59 +0300

    Update task_3.cpp

--- sources/task_3.cpp
+++ sources/task_3.cpp
@@ -1,8 +1,12 @@
-// Copyright 2018 Your Name <your_email>
-
+// Copyright 2018 Your Name<your_email>
 #include <tasks.hpp>
 
 // Task 3.
 void swap_el(std::vector<int>& input) {
-    // Fix it.
+  int num;
+  for (size_t i = 0; i < 3; ++i) {
+    num = input[i];
+    input[i] = input[17 + i];
+    input[17 + i] = num;
+  }
 }
