c53e9ef078b86b9a419751d4c249242e63cd3e8f
nadia <104782116+nadia4321@users.noreply.github.com>
2023-10-11 23:02:01 +0300

    Update task_4.cpp

--- sources/task_4.cpp
+++ sources/task_4.cpp
@@ -4,9 +4,25 @@
 
 // Task 4.
 void remove_negative(std::vector<int>& input) {
-    // Fix it.
+  for (size_t i = 0; i < input.size(); ++i) {
+    if (input[i] < 0) {
+      for (size_t j = i + 1; j < input.size(); ++j) {
+        input[j - 1] = input[j];
+      }
+      input.pop_back();
+      break;
+    }
+  }
 }
 
 void remove_last_even(std::vector<int>& input) {
-    // Fix it.
+  for (int i = input.size() - 1; i > -1; i -= 1) {
+    if (input[i] % 2 == 0) {
+      for (size_t j = i + 1; j < input.size(); ++j) {
+        input[j - 1] = input[j];
+      }
+      input.pop_back();
+      break;
+    }
+  }
 }
