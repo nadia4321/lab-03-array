ca68f3fc0dad75c8f92f115e3e39c2df9aa14f9e
nadia <104782116+nadia4321@users.noreply.github.com>
2023-10-11 22:57:11 +0300

    Update task_1.cpp

--- sources/task_1.cpp
+++ sources/task_1.cpp
@@ -2,18 +2,16 @@
 
 #include <tasks.hpp>
 
-
 // Task 1.
 void multi_2(std::vector<int>& input) {
-    for (size_t i = 0; i < input.size(); ++i) input[i] *= 2;
+  for (size_t i = 0; i < input.size(); ++i) input[i] *= 2;
 }
 
 void reduce_a(std::vector<int>& input, int a) {
-    for (size_t i = 0; i < input.size(); ++i) input[i] -= a;
+  for (size_t i = 0; i < input.size(); ++i) input[i] -= a;
 }
 
 void div_on_first(std::vector<int>& input) {
-    int a = input[0];
-    for (size_t i = 0; i < input.size(); ++i) input[i] /= a;
+  int a = input[0];
+  for (size_t i = 0; i < input.size(); ++i) input[i] /= a;
 }
-
